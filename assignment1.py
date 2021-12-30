7c3f4d2d37faaaa99335a7bed7aca23581979537
sidra <amirrehman842@gmail.com>
2021-12-31 02:29:21 +0500

    Assignment complete

---
+++ assignment1.py
@@ -0,0 +1,35 @@
+# Question no 1
+import math
+from datetime import datetime
+import sys
+print(''' twinkle twinkle little star,''')
+print('''           How I wonder what you are!''')
+print('''                   Up above the world so high,''')
+print('''                   Like a diamond in the sky.''')
+print(''' twinkle twinkle little star,''')
+print('''           How I wonder what you are''')
+
+# Question no 2
+print(sys.version)
+
+# Question no 3
+time = datetime.now()
+print(time)
+
+
+# Question no 4
+r = float(input("Ente radius of circle : "))
+area = math.pi * r * r
+print("Area of circle is : %.2f " % area)
+
+
+# Question no 5
+f_name = input(" Enter First name :")
+l_name = input(" Enter Last name : ")
+print(l_name + " " + f_name)
+
+# Question no 5
+v1 = int(input(" Enter value 1 : "))
+v2 = int(input(" Enter value 2 : "))
+add = v1 + v2
+print(add)
